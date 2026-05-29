218e84c5f4d8a058310194dc9db89a4f08917663
Rita Moreira <rita.moreira.1414@gmail.com>
2026-05-29 15:46:43 +0100

    otimização do codigo do ex3

--- src/main/scala/exercise3/tickets1.scala
+++ src/main/scala/exercise3/tickets1.scala
@@ -8,40 +8,43 @@ object TicketOfficeTest extends App {
 
     case class ToSell(n: Int)
     case class Buy(n: Int)
+    case object Bye
 
-    // Testing the system
+    class SellerActor extends Actor {
+        val log = Logging(context.system, this)
+        // estado inicial: 0 bilhetes disponíveis para venda
+        def receive: Actor.Receive = selling(0)
+        
+        // estado de venda, com n bilhetes disponíveis para venda
+        def selling(n: Int): Actor.Receive = {
+            case ToSell(m) => 
+                val newStock = n + m
+                log.info(s"Received to sell message: $m")
+                context.become(selling(newStock)) // adicionar m bilhetes à venda
+            case Buy(m) => 
+                val remainingStock = n - m
+                log.info(s"Received buy message: $m")
+                if (n >= m) context.become(selling(remainingStock)) // tem bilhetes restantes > 0
+                else log.error(s"Not enough tickets to sell $m tickets")
+            case Bye => 
+                log.info("Received bye message, stopping actor")
+                context.stop(self)
+            case _ => 
+                log.info("Received unknown message")
+        }
+    }
+
+    // testar
     val sys = akka.actor.ActorSystem("TicketSys")
     val ticketOffice = sys.actorOf(Props[SellerActor](), "main-office")
 
-    class SellerActor extends Actor {
-    val log = Logging(context.system, this)
-    // Initial state: no tickets (n=0)
-    def receive: Actor.Receive = selling(0)
-    
-    // Selling state: n tickets available
-    def selling(n: Int): Actor.Receive = {
-        case ToSell(m) => 
-            log.info(s"Received to sell message: $m")
-            context.become(selling(n + m)) // adiciona m bilhetes à venda
-        case Buy(m) => 
-            log.info(s"Received buy message: $m")
-            if (n >= m) context.become(selling(n - m)) // tem stock suficiente para vender m bilhetes
-            else log.error(s"Not enough tickets to sell $m tickets")
-        case "Bye" => 
-            log.info("Received bye message, stopping actor")
-            context.stop(self)
-        case _ => 
-            log.info("Received unknown message")
-    }
-    }
-
     ticketOffice ! ToSell(2000)
 
-    for (x <- 0 until 101) ticketOffice ! Buy(20)
+    for (_ <- 0 until 101) ticketOffice ! Buy(20)
 
     println(s"Tried to buy many ${20*101} tickets.") 
 
-    ticketOffice ! "Bye"
+    ticketOffice ! Bye
     Thread.sleep(3000)
 
     sys.terminate()
--- src/main/scala/exercise3/tickets2.scala
+++ src/main/scala/exercise3/tickets2.scala
@@ -1,6 +1,7 @@
 package exercise3
 import akka.actor._
 import akka.event.Logging
+import scala.io.StdIn
 // n > 100 then child Actor is created + send fixed number of tickets to sell (< 50)
 // child actors report to parent once they fail to have enough tickets to sell
 // returning the possible remaining tickets to sell
@@ -12,68 +13,103 @@ object TicketOfficeChild extends App {
     case class ToSell(n: Int)
     case class Buy(n: Int)
     case class Return(n: Int)
+    case object Bye
 
-    // Testing the system
-    val sys = akka.actor.ActorSystem("TicketSys")
-    // main-office: ator principal
-    val ticketOffice = sys.actorOf(Props[SellerActor](), "main-office")
+    class MainSellerActor extends Actor {
+        val log = Logging(context.system, this)
+        // Initial state: no tickets (n=0)
+        def receive: Actor.Receive = selling(0, Vector.empty)
 
-    class SellerActor extends Actor {
-    val log = Logging(context.system, this)
-    var rotateIdx = 0
-    // Initial state: no tickets (n=0)
-    def receive: Actor.Receive = selling(0)
-    
-    // Selling state: n tickets available
-    def selling(n: Int): Actor.Receive = {
-        case ToSell(m) =>
-            var stock = n+m 
-            log.info(s"Received to sell message: $m")
+        def excessStock(currentStock: Int, currentChildren: Vector[ActorRef]) : (Int, Vector[ActorRef]) = {
+            var stock = currentStock
+            var children = currentChildren
             while(stock > 100) {
-                val child = context.actorOf(Props[SellerActor]()) // cria novo ator (filho do SellerActor)
+                val child = context.actorOf(Props[ChildSellerActor]()) // cria novo ator (filho do MainSellerActor)
                 child ! ToSell(50) // passa valor fixo para tentar vender
+                children :+= child // adiciona o filho à lista de filhos do ator principal
                 stock -= 50
             }
-            context.become(selling(stock)) // atualiza o stock do ator principal
-        case Buy(m) => 
-            log.info(s"Received buy message: $m")
-            if (n >= m) context.become(selling(n - m)) // tem stock suficiente para vender m bilhetes
-            else {
-                val filhos = context.children.toList
-                if (filhos.isEmpty) {
-                    log.error(s"Not enough tickets to sell $m tickets")
-                    if (self.path.name != "main-office") { // quando o filho n tem stock suficiente, vai aos filhos dele - se nao tem, retorna ao pai
-                        log.info(s"Returning remaining stock of $n tickets to parent")
-                        context.parent ! Return(n) // retorna o stock restante para o pai
-                        context.stop(self) // para o ator atual
-                    }
-                } else {
-                    log.info(s"Not enough tickets to sell $m tickets, asking children for stock")
-                    // round robin
-                    if (rotateIdx >= filhos.size) rotateIdx = 0
-                    val designedChild = filhos(rotateIdx)
-                    designedChild.forward(Buy(m)) // encaminha a mensagem de compra para o filho designado
-                    rotateIdx = (rotateIdx + 1) % filhos.size // atualiza o índice para o próximo filho na próxima vez
+            (stock, children)
+        }
+        
+        // Selling state: n tickets available
+        def selling(n: Int, children: Vector[ActorRef]): Actor.Receive = {
+            case ToSell(m) =>
+                val (stock, updatedChildren) = excessStock(n + m, children) // atualiza o stock e a lista de filhos do ator principal
+                log.info(s"[Main] Received to sell message: $m, updated stock: $stock, children count: ${updatedChildren.size}")
+                context.become(selling(stock, updatedChildren)) // atualiza o stock do ator principal
+            case Buy(m) => 
+                log.info(s"[Main] Received buy message: $m")
+                if (n >= m) context.become(selling(n - m, children)) // tem stock suficiente para vender m bilhetes
+                else if (children.nonEmpty) {
+                    log.info(s"[Main] Not enough tickets to sell $m tickets, asking children for stock")
+                    children.head.forward(Buy(m)) // encaminha a mensagem de compra para o primeiro filho da lista
                 }
-            }
-        case Return(m) => 
-            log.info(s"Received return message: $m")
-            context.become(selling(n + m)) // atualiza o stock do ator principal com o valor retornado pelo filho
-        case "Bye" => 
-            log.info("Received bye message, stopping actor")
-            context.stop(self)
-        case _ => 
-            log.info("Received unknown message")
+                else {
+                    log.error(s"[Main] Not enough tickets to sell $m tickets and no children to ask for stock")
+                }
+            case Return(m) => 
+                val activeChildren = children.filterNot(_ == sender()) // remove o filho que retornou o stock
+                log.info(s"[Main] Received return message: $m from child, updating stock and children list")
+                val (updatedStock, updatedChildren) = excessStock(n + m, activeChildren) // atualiza o stock e a lista de filhos do ator principal
+                context.become(selling(updatedStock, updatedChildren)) // atualiza o stock do ator principal
+            case Bye => 
+                log.info("[Main] Received bye message, stopping actor")
+                context.stop(self)
+            case _ => 
+                log.info("[Main] Received unknown message")
+        }
     }
+
+    class ChildSellerActor extends Actor {
+        val log = Logging(context.system, this)
+        // estado inicial: 0 bilhetes disponíveis para venda
+        def receive: Actor.Receive = selling(0)
+
+        // estado de venda, com n bilhetes disponíveis para venda
+        def selling(n: Int): Actor.Receive = {
+            case ToSell(m) => 
+                val newStock = n + m
+                log.info(s"[Child] Received to sell message: $m, updated stock: $newStock")
+                context.become(selling(newStock)) // adicionar m bilhetes à venda
+            case Buy(m) => 
+                log.info(s"[Child] Received buy message: $m")
+                if (n >= m) context.become(selling(n - m)) // tem stock suficiente para vender m bilhetes
+                else {
+                    log.error(s"[Child] Not enough tickets to sell $m tickets, returning remaining stock to parent")
+                    context.parent ! Return(n) // retorna o stock restante para o pai
+                    context.parent.forward(Buy(m))
+                    context.become(rejecting)
+                }
+            case _ => 
+                log.info("[Child] Received unknown message")
+        }
+
+        def rejecting: Actor.Receive = {
+            case Buy(m) => 
+                log.error(s"[Child] Rejecting buy message: $m, not enough stock")
+                context.parent.forward(Buy(m)) // encaminha a mensagem de compra para o pai
+            case "AcknowledgeDeath" => 
+                log.info("[Child] Received acknowledge death message, stopping actor")
+                context.stop(self)
+        }
     }
 
-    ticketOffice ! ToSell(2000)
-
-    for (x <- 0 until 101) ticketOffice ! Buy(20)
-
-    println(s"Tried to buy many ${20*101} tickets.") 
+    // sistema de input de testes
+    print("Enter initial stock: ")
+    val initialStock = StdIn.readInt()
+    print("Enter tickets to buy: ")
+    val ticketsToBuy = StdIn.readInt()
+    print("Enter number of requests: ")
+    val numRequests = StdIn.readInt()
+    val sys = akka.actor.ActorSystem("TicketSys")
+    print("Starting...")
+    val ticketOffice = sys.actorOf(Props[MainSellerActor](), "main-office")
+    ticketOffice ! ToSell(initialStock)
+    for (_ <- 0 until numRequests) ticketOffice ! Buy(ticketsToBuy)
+    println(s"Tried to buy many ${ticketsToBuy*numRequests} tickets.")
     Thread.sleep(3000)
-    ticketOffice ! "Bye"
+    ticketOffice ! Bye
     Thread.sleep(1000)
     sys.terminate()
 }
